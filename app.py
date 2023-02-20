949219af65abdbcd3683a343a506411b359ff5fc
Martin Hlavacek <martin@martinhlavacek.cz>
2023-02-20 13:47:45 +0100

    feat(ci,foo): add github workflow and update foo service

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+PORT = os.getenv('PORT')
+FOO_PATH = os.getenv('FOO_PATH')
+MSG = os.getenv('MSG')
 
-@app.route('/foo')
+version = "v1.0.1"
+
+if PORT is None:
+    PORT = 3001
+
+if FOO_PATH is None:
+    FOO_PATH = "foo"
+
+if MSG is None:
+    MSG = "Hello foo"
+
+
+@app.route(f'/{FOO_PATH}')
 def hello_world():  # put application's code here
-    return 'Hello foo'
+    return {
+        "message": MSG,
+        "version": version
+    }
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
